5bffdff85bcdd427af94b26e5a3cb2ae0f71532c
imane <imanechadli2001@gmail.com>
2025-03-26 12:42:19 +0000

    ON-4 Feat : implementation de la fonctionnalite du limitation du taux

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,6 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
